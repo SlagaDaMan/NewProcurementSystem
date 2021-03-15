6079f4b65ec178523e913e0e8d9686fd9725ac46
SlagaDaMan <Lucky.mashigoane@gmail.com>
2021-03-15 17:35:42 +0200

    Add project files.

---
+++ Models/Assetco.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcurementSystem.Models
+{
+    public class Assetco
+    {
+        public int ApplicationType { get; set; }
+        public int CategoryType { get; set; }
+        public int Date { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int PersalNo { get; set; }
+        public int ComponentDetails{ get; set; }
+        public int EmailAddress { get; set; }
+        public int Unit { get; set; }
+        public int ResponsibleNumber{ get; set; }
+        public int PerimisesName { get; set; }
+        public int CovertOvert { get; set; }
+        public int BOSR { get; set; }
+        public int LossReference { get; set; }
+        public int RefDate { get; set; }
+        public int SerialNumber{ get; set; }
+        public int Description { get; set; }
+        public int SRNumber { get; set; }
+        public int AssetNo { get; set; }
+        public int TypeOfVehicle { get; set; }
+
+    }
+}
---
+++ Models/Dashboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcurementSystem.Models
+{
+    public class Dashboard
+    {
+        public int TotalApplications { get; set; }
+        public int PendingApplications { get; set; }
+        public int TotalAssets { get; set; }
+        public int TotalPO { get; set; }
+
+    }
+
+
+}
